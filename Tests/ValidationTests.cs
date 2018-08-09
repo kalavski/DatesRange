bb8b20af42672a2ef7223d4924fb4b9d443c8b52
DELL-PC\Dell <trax9518@gmail.com>
2018-08-09 13:36:06 +0200

    fix tests

--- Tests/ValidationTests.cs
+++ Tests/ValidationTests.cs
@@ -7,13 +7,13 @@ namespace myapp2
     [TestFixture]
     class ValidationTests
     {
-        [TestCase("01.02.2016", "02/07/2017", true)]
-        [TestCase("01/07", "02/07/2017", false)]
-        [TestCase("01/02/2017", "02/07/2017", true)]
+        [TestCase("01.02.2016", "02/07/2017", "true")]
+        [TestCase("01/07", "02/07/2017", "false")]
+        [TestCase("01/02/2017", "02/07/2017", "true")]
         public void Should_Return_Correct_String(string startDate, string finishDate, string testResult)
         {
             var result = Validation.Validation.IsValid(startDate, finishDate);
-            Assert.AreEqual(result, testResult);
+            Assert.AreEqual(result, Boolean.Parse(testResult));
         }
     }
 }
